8aad31eb6e6d70706a8c316cbfff9fb39a834543
ML Shaw <sha-w256wrx@MLs-Mac-Studio.local>
2025-09-01 11:15:23 -0500

    Deploy Sanity Studio to production - firstlastnerdom.sanity.studio

--- my-sanity-project/sanity.cli.ts
+++ my-sanity-project/sanity.cli.ts
@@ -5,6 +5,7 @@ export default defineCliConfig({
     projectId: 'df24xwzm',
     dataset: 'tftltn-dev-blog'
   },
+  studioHost: 'firstlastnerdom',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
